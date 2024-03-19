9ca9756bbbcecd849060b09e5c96e0ac78c474f3
Basharat Ali <basharatalidogar14@gmail.com>
2024-03-19 13:21:35 +0500

    edit readme and add new.md

--- src/pages/database-search/DatabaseSearch.jsx
+++ src/pages/database-search/DatabaseSearch.jsx
@@ -29,7 +29,7 @@ const handleSeeDetails = () => {
         <Sider>
           <SideNav />
         </Sider>
-      <div className="w-full">
+      <div className="w-[87%] md:w-full">
         {/* <div> <h1 className="mx-2 rounded-lg mt-2 md:mx-5 text-[22px] md:text-[24px] font-bold p-3 bg-[#ffffff] text-[#000000] capitalize">{window.location.pathname.substr(1,)}</h1>
         </div> */}
       <div className="main w-[100%] overflow-x-scroll">
