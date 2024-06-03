5a97fc512d8541372435ff0342948f8695e4e789
Basharat Ali <basharatalidogar14@gmail.com>
2024-06-03 15:58:07 +0500

    data added

--- src/pages/FbFanpage.jsx
+++ src/pages/FbFanpage.jsx
@@ -23,7 +23,7 @@ export default function FbFanpage() {
               <Content>
               <div className='main-body  flex flex-col gap-10'>
                     <div className='flex p-5 shadow-xl rounded-lg w-full flex-col lg:flex-row gap-5 lg:gap-3'>
-                        <div className='lg:w-[60%] flex flex-col gap-4'>
+                        <div className='lg:w-[60%] flex flex-col gap-3 sm:gap-4'>
                             <div className='flex flex-col md:flex-row gap-2 w-full'>
                                 <div className='flex flex-col gap-1 w-full'>
                                     <h3 className='text-[#030621] text-[14px] font-normal'>Fanpage URL</h3>
@@ -38,7 +38,7 @@ export default function FbFanpage() {
                                         <option value="">Three</option>
                                     </select>
                                 </div>
-                                <div className='flex gap-1 items-center mt-6 w-full'>
+                                <div className='flex gap-1 items-center sm:mt-6 w-full'>
                                     <h3 className='text-[14px] font-normal text-[#030621]'>Token Status:</h3> <div className=' ml-2 h-3 w-3 rounded-full shadow-xl bg-green'></div>
                                 </div>
                             </div>
@@ -52,9 +52,9 @@ export default function FbFanpage() {
                                 <div className='bg-[#fff] h-32 w-32 sm:h-24 sm:w-24 rounded-full'></div>
                             </div>
                             <div className='w-full flex flex-col gap-2'>
-                                <div className='flex w-full '>  <h3 className='text-[#030621] text-[14px] font-normal'>Facebook Fanpage</h3> <h4 className='font-bold text-[18px] text-[#000] ml-auto'>XXXX</h4> </div>
-                                <div className='flex w-full '>  <h3 className='text-[#030621] text-[14px] font-normal'>Total Posts Found</h3> <h4 className='font-bold text-[18px] text-[#000] ml-auto'>XXXX</h4> </div>
-                                <div className='flex w-full '>  <h3 className='text-[#030621] text-[14px] font-normal'>Total Leads Found</h3> <h4 className='font-bold text-[18px] text-[#000] ml-auto'>XXXX</h4> </div>
+                                <div className='flex w-full items-center '>  <h3 className='text-[#030621] text-[14px] font-normal'>Facebook Fanpage</h3> <h4 className='font-bold text-[18px] text-[#000] ml-auto'>XXXX</h4> </div>
+                                <div className='flex w-full items-center '>  <h3 className='text-[#030621] text-[14px] font-normal'>Total Posts Found</h3> <h4 className='font-bold text-[18px] text-[#000] ml-auto'>XXXX</h4> </div>
+                                <div className='flex w-full items-center '>  <h3 className='text-[#030621] text-[14px] font-normal'>Total Leads Found</h3> <h4 className='font-bold text-[18px] text-[#000] ml-auto'>XXXX</h4> </div>
                             </div>
                         </div>
                     </div>
@@ -104,7 +104,7 @@ export default function FbFanpage() {
                             </thead>
                             <tbody>
                                 <tr className='odd:bg-[#fff] even:bg-[#F5F5F5]'>
-                                    <td><div className='flex gap-3 items-center'><input type="checkbox" /><div><h3 className='text-[14px] md:text-[15px] font-medium text-[#000]'>Alex Summers</h3><p className='text-[12px] md:text-[13px] -mt-1'>CEO</p></div></div></td>
+                                    <td><div className='flex gap-3 items-center'><input type="checkbox" /><div><h3 className='font-medium text-[#000]'>Alex Summers</h3><p className=' -mt-1'>CEO</p></div></div></td>
                                     <td> <div className='flex gap-2 items-center'><div className='h-3 w-3 bg-green rounded-full shadow-xl'></div><p>alexsummers@gmail.com</p></div> </td>
                                     <td>Salesforce</td>
                                     <td>Male</td>
@@ -114,7 +114,7 @@ export default function FbFanpage() {
                                     <td>CEO</td>
                                 </tr>
                                 <tr className='odd:bg-[#fff] even:bg-[#F5F5F5]'>
-                                    <td><div className='flex gap-3 items-center'><input type="checkbox" /><div><h3 className='text-[14px] md:text-[15px] font-medium text-[#000]'>Alex Summers</h3><p className='text-[12px] md:text-[13px] -mt-1'>CEO</p></div></div></td>
+                                    <td><div className='flex gap-3 items-center'><input type="checkbox" /><div><h3 className='font-medium text-[#000]'>Alex Summers</h3><p className=' -mt-1'>CEO</p></div></div></td>
                                     <td> <div className='flex gap-2 items-center'><div className='h-3 w-3 bg-green rounded-full shadow-xl'></div><p>alexsummers@gmail.com</p></div> </td>
                                     <td>Salesforce</td>
                                     <td>Male</td>
@@ -124,7 +124,7 @@ export default function FbFanpage() {
                                     <td>CEO</td>
                                 </tr>
                                 <tr className='odd:bg-[#fff] even:bg-[#F5F5F5]'>
-                                    <td><div className='flex gap-3 items-center'><input type="checkbox" /><div><h3 className='text-[14px] md:text-[15px] font-medium text-[#000]'>Alex Summers</h3><p className='text-[12px] md:text-[13px] -mt-1'>CEO</p></div></div></td>
+                                    <td><div className='flex gap-3 items-center'><input type="checkbox" /><div><h3 className='font-medium text-[#000]'>Alex Summers</h3><p className=' -mt-1'>CEO</p></div></div></td>
                                     <td> <div className='flex gap-2 items-center'><div className='h-3 w-3 bg-green rounded-full shadow-xl'></div><p>alexsummers@gmail.com</p></div> </td>
                                     <td>Salesforce</td>
                                     <td>Male</td>
@@ -134,7 +134,7 @@ export default function FbFanpage() {
                                     <td>CEO</td>
                                 </tr>
                                 <tr className='odd:bg-[#fff] even:bg-[#F5F5F5]'>
-                                    <td><div className='flex gap-3 items-center'><input type="checkbox" /><div><h3 className='text-[14px] md:text-[15px] font-medium text-[#000]'>Alex Summers</h3><p className='text-[12px] md:text-[13px] -mt-1'>CEO</p></div></div></td>
+                                    <td><div className='flex gap-3 items-center'><input type="checkbox" /><div><h3 className='font-medium text-[#000]'>Alex Summers</h3><p className=' -mt-1'>CEO</p></div></div></td>
                                     <td> <div className='flex gap-2 items-center'><div className='h-3 w-3 bg-green rounded-full shadow-xl'></div><p>alexsummers@gmail.com</p></div> </td>
                                     <td>Salesforce</td>
                                     <td>Male</td>
@@ -144,7 +144,7 @@ export default function FbFanpage() {
                                     <td>CEO</td>
                                 </tr>
                                 <tr className='odd:bg-[#fff] even:bg-[#F5F5F5]'>
-                                    <td><div className='flex gap-3 items-center'><input type="checkbox" /><div><h3 className='text-[14px] md:text-[15px] font-medium text-[#000]'>Alex Summers</h3><p className='text-[12px] md:text-[13px] -mt-1'>CEO</p></div></div></td>
+                                    <td><div className='flex gap-3 items-center'><input type="checkbox" /><div><h3 className='font-medium text-[#000]'>Alex Summers</h3><p className=' -mt-1'>CEO</p></div></div></td>
                                     <td> <div className='flex gap-2 items-center'><div className='h-3 w-3 bg-green rounded-full shadow-xl'></div><p>alexsummers@gmail.com</p></div> </td>
                                     <td>Salesforce</td>
                                     <td>Male</td>
