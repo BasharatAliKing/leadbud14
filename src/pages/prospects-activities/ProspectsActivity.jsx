6fc51df334f09c68114569309905d4727ab3768d
Basharat Ali <basharatalidogar14@gmail.com>
2024-07-04 16:34:37 +0500

    edit readme and add new.md

--- src/pages/prospects-activities/ProspectsActivity.jsx
+++ src/pages/prospects-activities/ProspectsActivity.jsx
@@ -16,6 +16,8 @@ import { MdOutlineLock } from "react-icons/md";
 import { RiDeleteBinLine } from "react-icons/ri";
 import { IoSettingsOutline } from "react-icons/io5";
 import ButtonComponent from './ButtonComponent';
+import { HiOutlineSignal } from "react-icons/hi2";
+import { FaChevronRight } from "react-icons/fa";
 export default function ProspectsActivity() {
     const companykeywords = [
         "enterprise software",
@@ -69,7 +71,7 @@ export default function ProspectsActivity() {
                             </div>
                         </div>
                         <div className='w-full flex flex-col md:flex-row gap-5'>
-                            <div className='md:w-[65%] flex flex-col gap-6'>
+                            <div className='md:w-[60%] flex flex-col gap-6'>
                                 <div className='flex flex-col gap-3 p-5 bg-[#fff] shadow-xl rounded-xl'>
                                     <p className='text-[14px] xl:text-[15px] font-poppins text-[#7B7B7B]'>B2B and B2C software companies around the globe use Paddle to offload operational complexities so
                                         they can focus on growth....  <span className='text-[#48FA8A]'>Show More</span></p>
@@ -141,9 +143,93 @@ export default function ProspectsActivity() {
                                     <ButtonComponent />
                                 </div>
                             </div>
-                            <div className='md:w-[35%]'>
-                                <div className='flex justify-between shadow-xl bg-[#fff] p-5 rounded-md items-center'>
+                            <div className='md:w-[40%] flex flex-col gap-5'>
+                                <div className='flex flex-col gap-1 shadow-xl bg-[#fff] p-3 md:p-5 rounded-xl'>
+                                    <div className='flex justify-between w-full items-center'>
+                                        <h2 className='text-[15px] xl:text-[17px] font-semibold font-poppins text-[#030621]'>Signals & Communication Advice</h2>
+                                        <FaChevronDown className='' />
+                                    </div>
+                                    <h4 className='flex items-center gap-5 font-normal my-3 text-[#B7B7B7] text-[14px] md:text-[15px]'>Top signals for LinkedIn <IoSettingsOutline className='text-[#B7B7B7] text-[20px]' /></h4>
+                                    <div className='bg-[#F5F5F5] p-4 rounded-xl flex gap-3 w-full'>
+                                        <HiOutlineSignal className='text-[30px] text-[#48FA8A] mt-2' />
+                                        <div className='w-full'>
+                                            <div className='flex justify-between w-full items-center'>
+                                                <h2 className='text-[15px] xl:text-[16px] font-semibold font-poppins text-[#030621]'>Award or recognition</h2>
+                                                <FaChevronRight className='text-[#030621]' />
+                                            </div>
+                                            <h4 className='text-[14px] xl:text-[15px] font-normal font-poppins text-[#7B7B7B]'>Recognition = Recognition +1</h4>
+                                            <p className='mt-5 text-[13px] xl:text-[14px] font-normal font-poppins text-[#7B7B7B]'>Discuss the recent award or recognition</p>
+                                        </div>
+                                    </div>
+                                    <div className=' p-4 rounded-xl flex gap-3 w-full'>
+                                        <HiOutlineSignal className='text-[30px] text-[#48FA8A] mt-2' />
+                                        <div className='w-full'>
+                                            <div className='flex justify-between w-full items-center'>
+                                                <h2 className='text-[15px] xl:text-[16px] font-semibold font-poppins text-[#030621]'>New Partnership</h2>
+                                                <FaChevronRight className='text-[#030621]' />
+                                            </div>
+                                            <h4 className='text-[14px] xl:text-[15px] font-normal font-poppins text-[#7B7B7B]'>Recognition = Recognition +1</h4>
+                                            <p className='mt-5 text-[13px] xl:text-[14px] font-normal font-poppins text-[#7B7B7B]'>Congratulate them on the new partnership</p>
+                                        </div>
+                                    </div>
+                                    <div className='bg-[#F5F5F5] p-4 rounded-xl flex gap-3 w-full'>
+                                        <HiOutlineSignal className='text-[30px] text-[#48FA8A] mt-2' />
+                                        <div className='w-full'>
+                                            <div className='flex justify-between w-full items-center'>
+                                                <h2 className='text-[15px] xl:text-[16px] font-semibold font-poppins text-[#030621]'>New product or service</h2>
+                                                <FaChevronRight className='text-[#030621]' />
+                                            </div>
+                                            <h4 className='text-[14px] xl:text-[15px] font-normal font-poppins text-[#7B7B7B]'>New Offering = New Offering +1</h4>
+                                            <p className='mt-5 text-[13px] xl:text-[14px] font-normal font-poppins text-[#7B7B7B]'>Congratulate them on the new offering and</p>
+                                        </div>
+                                    </div>
+                                    <div className='flex mt-3 items-center text-[#030621]'>
+                                        <input type="checkbox" />
+                                        <h3 className='text-[13px] xl:text-[14px] font-normal ml-1'>Hide talking tips & snippets</h3>
+                                    </div>
+                                </div>
+                                <div className='flex flex-col gap-4 shadow-xl bg-[#fff] p-3 md:p-5 rounded-xl'>
+                                    <div className='flex flex-col gap-4 p-3 bg-[#F5F5F5] rounded-xl'>
+                                        <div className='flex justify-between items-center'>
+                                            <h2 className='text-[16px] xl:text-[18px] text-[#030621] font-semibold font-poppins'>Activities</h2>
+                                            <h5 className='text-[14px] xl:text-[15px] text-[#48FA8A] font-normal font-poppins cursor-pointer'>Hide Activities</h5>
+                                        </div>
+                                        <div className='flex gap-3'>
+                                            <div className='flex gap-1 items-center'><input type="radio" name='selecttype' /><p className='text-[13px] font-normal text-[#030621] md:text-[14px]'>Note</p></div>
+                                            <div className='flex gap-1 items-center'><input type="radio" name='selecttype' /><p className='text-[13px] font-normal text-[#030621] md:text-[14px]'>Call</p></div>
+                                            <div className='flex gap-1 items-center'><input type="radio" name='selecttype' /><p className='text-[13px] font-normal text-[#030621] md:text-[14px]'>Action</p></div>
+                                            <div className='flex gap-1 items-center'><input type="radio" name='selecttype' /><p className='text-[13px] font-normal text-[#030621] md:text-[14px]'>Task</p></div>
+                                        </div>
+                                        <textarea name="" id="" rows={3} placeholder='Type a Note.....' className='outline-none border-none bg-[#fff] rounded-xl placeholder-[#B7B7B7] text-[#B7B7B7] text-[14px] w-full'></textarea>
+                                        <div className='flex justify-between items-center'>
+                                            <button className='bg-[#fff] py-2 text-[#B7B7B7] text-[14px] xl:text-[15px] font-normal px-4'>Log Note</button>
+                                            <div className='flex gap-1 items-center'><input type="checkbox" className='' /><p className='text-[13px] font-normal text-[#030621] md:text-[14px]'>Pin note to top</p></div>
+                                        </div>
 
+                                    </div>
+                                    <div className='flex flex-col gap-1 p-3 bg-[#F5F5F5] rounded-xl'>
+                                        <div className='flex justify-between items-center'>
+                                            <h2 className='text-[16px] xl:text-[18px] text-[#030621] font-semibold font-poppins'>Activities</h2>
+                                            <h5 className='text-[14px] xl:text-[15px] text-[#48FA8A] font-normal font-poppins cursor-pointer flex gap-3 items-center'>All Activity types <FaChevronDown /> </h5>
+                                        </div>
+                                        <p className='text-[14px] mt-4 xl:text-[15px] font-poppins font-normal text-[#7B7B7B]'>Leadbud automatic email fulfillment found a
+                                            verified email hannah.loughlin@paddle.com
+                                            for Hannah L.
+                                        </p>
+                                        <p className='text-[14px] xl:text-[15px] font-poppins font-normal text-[#7B7B7B]'> 05:35 PM</p>
+                                        <p className='text-[14px] mt-4 xl:text-[15px] font-poppins font-normal text-[#7B7B7B]'>
+                                            The account has been automatically
+                                            enriched with industry, company size, and
+                                            other demographics. information with
+                                            Paddie in Apollo's company database
+                                        </p>
+                                        <p className='text-[14px] xl:text-[15px] font-poppins font-normal text-[#7B7B7B]'> 05:35 PM</p>
+                                        <p className='text-[14px] mt-4 xl:text-[15px] font-poppins font-normal text-[#7B7B7B]'>
+                                            Requested from Apollo by Aisha
+                                        </p>
+                                        <p className='text-[14px] xl:text-[15px] font-poppins font-normal text-[#7B7B7B]'> 05:35 PM</p>
+                                    </div>
+                                    <p className='text-center text-[#48FA8A] font-poppins font-normal text-[14px] cursor-pointer'>Show in Window View</p>
                                 </div>
                             </div>
                         </div>
