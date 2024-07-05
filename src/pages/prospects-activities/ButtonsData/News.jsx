32495c2aa140992583a010cb4937e6dde3398deb
Basharat Ali <basharatalidogar14@gmail.com>
2024-07-05 15:37:24 +0500

    edit readme and add new.md

--- src/pages/prospects-activities/ButtonsData/News.jsx
+++ src/pages/prospects-activities/ButtonsData/News.jsx
@@ -11,34 +11,34 @@ export default function News() {
                    <div className='bg-[rgb(245,245,245)] text-[#B7B7B7] rounded-md p-3 px-4 text-[14px] md:text-[16px] flex items-center gap-1'><FiFilter className='font-semibold text-[20px]'/> Filter</div>
                    <div className='bg-[rgb(245,245,245)] text-[#B7B7B7] rounded-md p-1 px-4 text-[14px] md:text-[16px] flex items-center gap-1 w-full sm:w-auto'><CiSearch className='text-[22px]'/><input type="search" placeholder='Search' className='outline-none placeholder-[#B7B7B7] w-full border-none bg-[rgb(245,245,245)]' /> </div>
                 </div>
-                <div className="p-2 text-[13px] md:-2 flex flex-col bg-[rgb(245,245,245)] rounded-md whitespace-nowrap overflow-scroll lg:overflow-auto">
+                <div className="p-2 text-[13px] md:-2 flex flex-col bg-[rgb(245,245,245)] rounded-md gap-1 sm:gap-0 ">
                     <p className="text-[14px] md:text-[16px] font-normal font-poppins text-[#030621]">City attraction ranked one of the best in the world</p>
-                    <div className="text-[15px] text-[#7B7B7B] font-normal font-poppins flex gap-5">
+                    <div className="text-[15px] text-[#7B7B7B] font-normal font-poppins flex flex-col sm:flex-row whitespace-nowrap gap-x-3 sm:gap-x-0 gap-1 sm:gap-5">
                         <h1 className='flex gap-2 text-[#48FA8A]'><IoIosLink className='text-[20px]' /> eveningnews24.co.uk</h1>
                         <h2 className='font-[300]'>Receives Award</h2>
                         <h3>Sep 25, 2023</h3>
                     </div>
                 </div>
 
-                <div className="p-2 flex flex-col whitespace-nowrap overflow-scroll lg:overflow-auto">
+                <div className="p-2 flex flex-col gap-1 sm:gap-0 ">
                     <p className="text-[13px] md:text-[15px]  font-normal font-poppins text-[#030621]">Paddle announces Paddle Billing API set for SaaS companies</p>
-                    <div className="text-[15px] text-[#7B7B7B] font-normal font-poppins flex gap-5">
+                    <div className="text-[15px] text-[#7B7B7B] font-normal font-poppins flex flex-col sm:flex-row whitespace-nowrap gap-1 gap-x-3 sm:gap-x-0 sm:gap-5">
                         <h1 className='flex gap-2 text-[#48FA8A]'><IoIosLink className='text-[20px]' /> google.com</h1>
                         <h2 className='font-[300]'>Launches</h2>
                         <h3>Aug 11, 2023</h3>
                     </div> </div>
-                <div className="p-2 text-[13px] md:-2 flex flex-col bg-[rgb(245,245,245)] rounded-md whitespace-nowrap lg:whitespace-normal overflow-scroll lg:overflow-auto">
+                <div className="p-2 text-[13px] md:-2 flex flex-col bg-[rgb(245,245,245)] rounded-md gap-1 sm:gap-0">
                     <p className="text-[14px] md:text-[16px] font-normal font-poppins text-[#030621] lg:w-3/4">Payments infrastructure provider paddles launches $10M Al program to
                         support software founders</p>
-                    <div className="text-[15px] text-[#7B7B7B] font-normal font-poppins flex gap-5">
+                    <div className="text-[15px] text-[#7B7B7B] font-normal font-poppins flex flex-col sm:flex-row gap-1 sm:gap-5">
                         <h1 className='flex gap-2 text-[#48FA8A]'><IoIosLink className='text-[20px]' /> mpost.io</h1>
                         <h2 className='font-[300]'>Launches</h2>
                         <h3>Jul 18, 2023</h3>
                     </div>   </div>
 
-                <div className="p-2 flex flex-col whitespace-nowrap overflow-scroll lg:overflow-auto">
+                <div className="p-2 flex flex-col gap-1 sm:gap-0 ">
                     <p className="text-[13px] md:text-[15px]  font-normal font-poppins text-[#030621]">Apple competitors set to launch rival App Stores following DMA mandate</p>
-                    <div className="text-[15px] text-[#7B7B7B] font-normal font-poppins flex gap-5">
+                    <div className="text-[15px] text-[#7B7B7B] font-normal font-poppins flex flex-col sm:flex-row gap-1 sm:gap-5">
                         <h1 className='flex gap-2 text-[#48FA8A]'><IoIosLink className='text-[20px]' /> itworldcanada.com</h1>
                         <h2 className='font-[300]'>Launches</h2>
                         <h3>Dec 20, 2023</h3>
--- src/pages/prospects-activities/ProspectsActivity.jsx
+++ src/pages/prospects-activities/ProspectsActivity.jsx
@@ -146,41 +146,41 @@ export default function ProspectsActivity() {
                             <div className='lg:w-[40%] flex flex-col gap-5'>
                                 <div className='flex flex-col gap-1 shadow-xl bg-[#fff] p-3 md:p-5 rounded-xl'>
                                     <div className='flex justify-between w-full items-center'>
-                                        <h2 className='text-[15px] xl:text-[17px] font-semibold font-poppins text-[#030621]'>Signals & Communication Advice</h2>
-                                        <FaChevronDown className='' />
+                                        <h2 className='text-[14px] sm:text-[15px] xl:text-[17px] font-semibold font-poppins text-[#030621]'>Signals & Communication Advice</h2>
+                                        <FaChevronDown className='w-[16px] sm:w-auto' />
                                     </div>
                                     <h4 className='flex items-center gap-5 font-normal my-3 text-[#B7B7B7] text-[14px] md:text-[15px]'>Top signals for LinkedIn <IoSettingsOutline className='text-[#B7B7B7] text-[20px]' /></h4>
                                     <div className='bg-[#F5F5F5] p-4 rounded-xl flex gap-3 w-full'>
                                         <HiOutlineSignal className='text-[30px] text-[#48FA8A] mt-2' />
                                         <div className='w-full'>
                                             <div className='flex justify-between w-full items-center'>
-                                                <h2 className='text-[15px] xl:text-[16px] font-semibold font-poppins text-[#030621]'>Award or recognition</h2>
-                                                <FaChevronRight className='text-[#030621]' />
+                                                <h2 className='text-[14px] sm:text-[15px] xl:text-[16px] font-semibold font-poppins text-[#030621]'>Award or recognition</h2>
+                                                <FaChevronRight className=' text-[#030621]' />
                                             </div>
-                                            <h4 className='text-[14px] xl:text-[15px] font-normal font-poppins text-[#7B7B7B]'>Recognition = Recognition +1</h4>
-                                            <p className='mt-5 text-[13px] xl:text-[14px] font-normal font-poppins text-[#7B7B7B]'>Discuss the recent award or recognition</p>
+                                            <h4 className='text-[13px] sm:text-[14px] xl:text-[15px] font-normal font-poppins text-[#7B7B7B]'>Recognition = Recognition +1</h4>
+                                            <p className='mt-3 sm:mt-5 text-[12px] sm:text-[13px] xl:text-[14px] font-normal font-poppins text-[#7B7B7B]'>Discuss the recent award or recognition</p>
                                         </div>
                                     </div>
                                     <div className=' p-4 rounded-xl flex gap-3 w-full'>
                                         <HiOutlineSignal className='text-[30px] text-[#48FA8A] mt-2' />
                                         <div className='w-full'>
                                             <div className='flex justify-between w-full items-center'>
-                                                <h2 className='text-[15px] xl:text-[16px] font-semibold font-poppins text-[#030621]'>New Partnership</h2>
+                                                <h2 className='text-[14px] sm:text-[15px] xl:text-[16px] font-semibold font-poppins text-[#030621]'>New Partnership</h2>
                                                 <FaChevronRight className='text-[#030621]' />
                                             </div>
-                                            <h4 className='text-[14px] xl:text-[15px] font-normal font-poppins text-[#7B7B7B]'>Recognition = Recognition +1</h4>
-                                            <p className='mt-5 text-[13px] xl:text-[14px] font-normal font-poppins text-[#7B7B7B]'>Congratulate them on the new partnership</p>
+                                            <h4 className='text-[13px] sm:text-[14px] xl:text-[15px] font-normal font-poppins text-[#7B7B7B]'>Recognition = Recognition +1</h4>
+                                            <p className='mt-3 sm:mt-5 text-[12px] sm:text-[13px] xl:text-[14px] font-normal font-poppins text-[#7B7B7B]'>Congratulate them on the new partnership</p>
                                         </div>
                                     </div>
                                     <div className='bg-[#F5F5F5] p-4 rounded-xl flex gap-3 w-full'>
                                         <HiOutlineSignal className='text-[30px] text-[#48FA8A] mt-2' />
                                         <div className='w-full'>
                                             <div className='flex justify-between w-full items-center'>
-                                                <h2 className='text-[15px] xl:text-[16px] font-semibold font-poppins text-[#030621]'>New product or service</h2>
+                                                <h2 className='text-[14px] sm:text-[15px] xl:text-[16px] font-semibold font-poppins text-[#030621]'>New product or service</h2>
                                                 <FaChevronRight className='text-[#030621]' />
                                             </div>
-                                            <h4 className='text-[14px] xl:text-[15px] font-normal font-poppins text-[#7B7B7B]'>New Offering = New Offering +1</h4>
-                                            <p className='mt-5 text-[13px] xl:text-[14px] font-normal font-poppins text-[#7B7B7B]'>Congratulate them on the new offering and</p>
+                                            <h4 className='text-[13px] sm:text-[14px] xl:text-[15px] font-normal font-poppins text-[#7B7B7B]'>New Offering = New Offering +1</h4>
+                                            <p className='mt-3 sm:mt-5 text-[12px] sm:text-[13px] xl:text-[14px] font-normal font-poppins text-[#7B7B7B]'>Congratulate them on the new offering and</p>
                                         </div>
                                     </div>
                                     <div className='flex mt-3 items-center text-[#030621]'>
