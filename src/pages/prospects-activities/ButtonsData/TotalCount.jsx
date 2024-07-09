1d07a314420534292e95c1edcdbbc85236b17a9c
Basharat Ali <basharatalidogar14@gmail.com>
2024-07-09 12:34:46 +0500

    edit readme and add new.md

--- src/pages/prospects-activities/ButtonsData/TotalCount.jsx
+++ src/pages/prospects-activities/ButtonsData/TotalCount.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export default function TotalCount() {
   return (
-    <div>TotalCount</div>
+    <>
+    <div>
+        <div className='flex flex-wrap md:gap-7 justify-center'>
+            <div className='flex flex-col items-center'>
+                <h2 className='text-[#030621] text-[20px] font-semibold'>2.9%</h2>
+                <h5 className='text-[#7B7B7B] text-[13px] font-normal'>6 Month Growth</h5>
+            </div>
+            <div className='flex flex-col items-center'>
+                <h2 className='text-[#030621] text-[20px] font-semibold'>5.1%</h2>
+                <h5 className='text-[#7B7B7B] text-[13px] font-normal'>1 Year Growth</h5>
+            </div>
+            <div className='flex flex-col items-center'>
+                <h2 className='text-[#030621] text-[20px] font-semibold'>14.0%</h2>
+                <h5 className='text-[#7B7B7B] text-[13px] font-normal'>2 Year Growth</h5>
+            </div>
+        </div>
+         {/* total chart */}
+        <div>
+
+        </div>
+        <div></div>
+    </div>
+    </>
   )
 }
